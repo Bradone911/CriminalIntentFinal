045f4ca352cfdfdad72957a9d8e411623e440f18
Bradone911 <baholland1@buffs.wtamu.edu>
2018-02-25 22:53:09 -0600

    chapter 10 initial

--- app/src/main/java/com/bignerdranch/android/criminalintentfinal/CrimeActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintentfinal/CrimeActivity.java
@@ -1,8 +1,22 @@
 package com.bignerdranch.android.criminalintentfinal;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 
+import java.util.UUID;
+
 public class CrimeActivity extends SingleFragmentActivity {
+
+    public static final String EXTRA_CRIME_ID =
+            "com.bignerdranch.android.criminalintentfinal.crime.id";
+
+    public static Intent newIntent(Context packageContext, UUID crimeID)    {
+        Intent intent = new Intent(packageContext, CrimeActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeID);
+
+        return intent;
+    }
 
     @Override
     protected Fragment createFragment() {
