70d47b2ab7f9882201f5f815ff5c8da82f6604ed
Bradone911 <baholland1@buffs.wtamu.edu>
2018-03-27 00:07:23 -0500

    Chapter 13

--- app/src/main/java/com/bignerdranch/android/criminalintentfinal/CrimeLab.java
+++ app/src/main/java/com/bignerdranch/android/criminalintentfinal/CrimeLab.java
@@ -23,12 +23,16 @@ public class CrimeLab {
     //       loaded model layer with 100 crimes
     private CrimeLab(Context context)   {
         mCrimes = new ArrayList<>();
-        for (int i = 0; i < 100; i++)   {
-            Crime crime = new Crime();
-            crime.setTitle("Crime #" + i);
-            crime.setSolved(i % 2 == 0);//every other one
-            mCrimes.add(crime);
-        }
+//        for (int i = 0; i < 100; i++)   {
+//            Crime crime = new Crime();
+//            crime.setTitle("Crime #" + i);
+//            crime.setSolved(i % 2 == 0);//every other one
+//            mCrimes.add(crime);
+//        }
+    }
+
+    public void addCrime(Crime c)   {
+        mCrimes.add(c);
     }
 
     public List<Crime> getCrimes()  {
--- app/src/main/java/com/bignerdranch/android/criminalintentfinal/CrimeListFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintentfinal/CrimeListFragment.java
@@ -3,9 +3,13 @@ package com.bignerdranch.android.criminalintentfinal;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -17,17 +21,30 @@ import java.util.List;
 
 public class CrimeListFragment extends Fragment {
 
+    private static final String SAVED_SUBTITLE_VISIBLE = "subtitle";
+
     private RecyclerView mCrimeRecyclerView;
     private CrimeAdapter mAdapter;
+    private boolean mSubtitleVisible;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-        Bundle savedInstanceState)  {
+                             Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
 
         mCrimeRecyclerView = (RecyclerView) view
                 .findViewById(R.id.crime_recycler_view);
         mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        if (savedInstanceState != null) {
+            mSubtitleVisible = savedInstanceState.getBoolean(SAVED_SUBTITLE_VISIBLE);
+        }
 
         updateUI();
 
@@ -35,9 +52,61 @@ public class CrimeListFragment extends Fragment {
     }
 
     @Override
-    public void onResume()  {
+    public void onResume() {
         super.onResume();
         updateUI();
+    }
+
+//    @Override  the book code has this here, why does it work without it???
+    public void onSavedInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(SAVED_SUBTITLE_VISIBLE, mSubtitleVisible);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_crime_list, menu);
+
+        MenuItem subtitleItem = menu.findItem(R.id.show_subtitle);
+    if (mSubtitleVisible){
+        subtitleItem.setTitle(R.string.hide_subtitle);
+    }else {
+        subtitleItem.setTitle(R.string.show_subtitle);
+    }
+}
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)  {
+        switch (item.getItemId())   {
+            case R.id.new_crime:
+                Crime crime = new Crime();
+                CrimeLab.get(getActivity()).addCrime(crime);
+                Intent intent = CrimePagerActivity
+                        .newIntent(getActivity(), crime.getId());
+                startActivity(intent);
+                return true;
+            case R.id.show_subtitle:
+                mSubtitleVisible = !mSubtitleVisible;
+                getActivity().invalidateOptionsMenu();
+                updateSubTitle();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void updateSubTitle()   {
+        CrimeLab crimeLab = CrimeLab.get(getActivity());
+        int crimeCount = crimeLab.getCrimes().size();
+        String subtitle = getString(R.string.subtitle_format, crimeCount);
+
+        if (!mSubtitleVisible) {
+            subtitle = null;
+        }
+
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        activity.getSupportActionBar().setSubtitle(subtitle);
     }
 
     private void updateUI() {
@@ -50,6 +119,8 @@ public class CrimeListFragment extends Fragment {
         } else {
             mAdapter.notifyDataSetChanged();
         }
+
+        updateSubTitle();
     }
 
     private class CrimeHolder extends RecyclerView.ViewHolder
